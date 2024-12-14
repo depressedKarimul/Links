60ee8d84aaedbe259720c1b0c85f2eb2bf8d405b
karimul-hassan <karimul.hassan09@gmail.com>
2024-12-14 09:15:29 +0600

    Implement JavaScript functionality for dark mode and light mode toggle

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-  // Initialize tooltips
-  tippy('.link', {
+// Initialize tooltips
+tippy('.link', {
     placement: 'bottom'
 });
 
@@ -11,6 +11,7 @@ const profile = document.getElementById('profile');
 // Default dark mode
 body.classList.add('text-gray-100'); // Dark text
 profile.classList.add('bg-gray-900'); // Dark background
+profile.style.backgroundImage = "url('https://img.freepik.com/free-vector/dark-hexagonal-background-with-gradient-color_79603-1409.jpg?t=st=1734144797~exp=1734148397~hmac=b791701378c100849a66c4183c41e969ec7aace4542d43d49c2d7a68cc2145c0&w=996')"; // Default dark background image
 
 // Toggle theme on button click
 toggle.addEventListener('click', () => {
@@ -21,6 +22,7 @@ toggle.addEventListener('click', () => {
         body.classList.add('text-gray-900');
         profile.classList.remove('bg-gray-900');
         profile.classList.add('bg-white');
+        profile.style.backgroundImage = "url('https://img.freepik.com/free-vector/realistic-white-monochrome-background_23-2149010358.jpg?t=st=1734144920~exp=1734148520~hmac=22cf2c3a354077a4e0266a66fef863f50eaaaf4485b54f4bca23f79e3ba0f8af&w=996')"; // Light background image
     } else {
         // Switch to dark mode
         toggle.innerHTML = "☀️";
@@ -28,7 +30,6 @@ toggle.addEventListener('click', () => {
         body.classList.add('text-gray-100');
         profile.classList.remove('bg-white');
         profile.classList.add('bg-gray-900');
+        profile.style.backgroundImage = "url('https://img.freepik.com/free-vector/dark-hexagonal-background-with-gradient-color_79603-1409.jpg?t=st=1734144797~exp=1734148397~hmac=b791701378c100849a66c4183c41e969ec7aace4542d43d49c2d7a68cc2145c0&w=996')"; // Dark background image
     }
 });
-
-		
